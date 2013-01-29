15c3d44e0d1e9593202d907f9e5992b6373429a5
Ian Young <pirate@gmail.com>
2013-01-28 22:08:50 -0800

    Cant test routes thanks to devise. Stopping

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  # We can't test routes because of the devise crap
+  describe 'GET new' do
+
+    describe 'success' do
+
+    end
+  end
+end
