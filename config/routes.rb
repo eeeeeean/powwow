ee0674d2505cf2a2191efcf8c6a7f960616e17da
Ian Young <pirate@gmail.com>
2013-01-28 20:59:40 -0800

    Initialized devise and added its crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Powwow::Application.routes.draw do
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,8 +50,6 @@ Powwow::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
