048bda50b17d729ac97c424b976bcaf45de2b082
Ian Young <pirate@gmail.com>
2013-02-03 22:43:37 -0800

    Gotta do something about these .swp files

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -10,4 +10,9 @@ describe Group do
     no_name_group = Group.new(@attr.merge(name: ""))
     no_name_group.should_not be_valid
   end
+
+  it 'should create a vanilla group' do
+    vanilla_group = Group.new(@attr)
+    vanilla_group.should be_valid
+  end
 end
